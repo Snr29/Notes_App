ae05d576c515ee92537c47941dc13ae79f7d885a
Snr29 <sivanageswararao29@gmail.com>
2023-01-23 11:36:24 +0530

    solution

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Notes from "./components/pages/Notes";
+import CreateNote from "./components/pages/CreateNote";
+import EditNote from "./components/pages/EditNote";
+//import dummyNotes from "./dummy_notes";
+
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes")) || []
+  );
+  //console.log(notes);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  return (
+    <main id="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Notes notes={notes} />} />
+          <Route
+            path="/create-note"
+            element={<CreateNote setNotes={setNotes} />}
+          />
+          <Route
+            path="/edit-note/:id"
+            element={<EditNote notes={notes} setNotes={setNotes} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
+};
+
+export default App;
